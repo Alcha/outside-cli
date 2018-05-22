7dcadae634e9a509900e20c93af1925a384573f4
Devin Leaman <dleaman212@gmail.com>
2018-05-22 18:59:19 -0500

    Initial adding of all code

---
+++ cmds/forecast.js
@@ -0,0 +1,23 @@
+const ora = require('ora')
+const getWeather = require('../util/weather')
+const getLocation = require('../util/location')
+
+module.exports = async (args) => {
+  const spinner = ora().start()
+
+  try {
+    const location = args.location || args.l || await getLocation()
+    const weather = await getWeather(location)
+
+    spinner.stop()
+
+    console.log(`Forecast for ${location}:`)
+
+    weather.forecast.forEach(item =>
+      console.log(`\t${item.date} - Low: ${item.low}° | High: ${item.high}° | ${item.text}`)
+    )
+  } catch (err) {
+    spinner.stop()
+    console.error(err)
+  }
+}
